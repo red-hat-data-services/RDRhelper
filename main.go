97b3857fd7cf5dade4ca80ebdfeccefe67378e0f
Chris Blum <cblum@redhat.com>
2021-03-31 13:54:40 +0200

    Log only to file

--- main.go
+++ main.go
@@ -11,6 +11,7 @@ import (
 var pages = tview.NewPages()
 var app = tview.NewApplication()
 var logMultiWriter io.Writer
+var logFile *os.File
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{
@@ -21,12 +22,11 @@ func init() {
 }
 
 func main() {
-	logFile, e := os.Create("asyncDRhelper.log")
-	if e != nil {
-		panic(e)
+	logFile, err := os.Create("asyncDRhelper.log")
+	if err != nil {
+		panic(err)
 	}
-	logMultiWriter = io.MultiWriter(os.Stdout, logFile)
-	log.SetOutput(logMultiWriter)
+	log.SetOutput(logFile)
 
 	pages.AddAndSwitchToPage("main",
 		tview.NewList().
@@ -35,7 +35,13 @@ func main() {
 			AddItem("Configure Primary", "Configure PVs for DR on the primary side", '3', func() { populatePrimaryPVCs(); pages.SwitchToPage("configurePrimary") }).
 			AddItem("Configure Secondary", "Configure PVs for DR on the secondary side", '4', nil).
 			AddItem("Configure Kubeconfigs", "Configure which Kubeconfigs to use for primary and secondary locations", '5', func() { showConfigPage() }).
-			AddItem("Failover / Failback", "Failover to secondary or Failback to primary location", '9', nil).
+			AddItem("Failover / Failback", "Failover to secondary or Failback to primary location", '9', func() {
+				log.Info("Selected 9")
+				_, err := getCephStatus(kubeConfigPrimary)
+				if err != nil {
+					log.WithError(err).Warn("ISSUE")
+				}
+			}).
 			AddItem("Quit", "Press to exit app", 'q', func() { app.Stop() }),
 		true)
 
